9fc7d564a48a8feb400c926d56b946cac811471a
JojoLeCodeur <37811790+JojoLeCodeur@users.noreply.github.com>
2018-04-01 11:51:50 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PHP echappate characters</title>
+	</head>
+	<body>
+		Taper du texte pour échapper les caractères
+		<form method="post" action="echappate-characters.php" autocomplete="off">
+			<textarea cols="100" rows="8" name="text"></textarea>
+			<input type="submit" value="Echapper les caractères" name="form">
+		</form>
+	</body>
+</html>
